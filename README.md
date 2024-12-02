eb53431cb23596258af54755995ddfcff36813b5
thomasmcconnell <tpm5746@psu.edu>
2024-12-01 20:47:10 -0500

    added a bunch of stuff joe added

---
+++ README.md
@@ -0,0 +1,17 @@
+# Voynich Manuscript Decoder
+
+A Java application for analyzing and decrypting the Voynich manuscript using frequency analysis and Caesar cipher methods.
+
+## Features
+- Frequency analysis for multiple languages (English, Italian, Latin, Spanish).
+- Decryption using Caesar cipher brute force.
+- Read ciphertext input from a file.
+
+## Usage
+1. Place the ciphertext in a file named `vciphertext.txt` in the projects folder
+2. Run the program in IntelliJ.
+3. View the results in the console.
+
+## Requirements
+- Java 8 or later
+- IntelliJ IDEA
--- Voynich.java
+++ Voynich.java
@@ -1,57 +1,59 @@
-/**
- * Project: Voynich Manuscript
- * Course: IST 242
- * Author: Thomas McConnell
- * Date Developed: 11/24/24
- * Last Date Changed: 11/26/24
- */
-//websites i used to get the manuscript, some info, and the table to translate to english letters \/
-//https://collections.library.yale.edu/catalog/2002046
-//https://en.m.wikipedia.org/wiki/Voynich_manuscript#/media/File%3AVoynich_EVA.svg
-//https://en.m.wikipedia.org/wiki/Voynich_manuscript
+import java.io.*;
 import java.util.*;
 
 public class Voynich {
 
     /**
      * Main method to run the program.
-     * It processes the ciphertext, analyzes letter frequencies, and applies decryption methods.
+     * It processes the ciphertext, analyzes letter frequencies, and applies decryption methods
+     * for English, Latin, Spanish, and Italian.
      *
      * @param args Command-line arguments.
      */
-
     public static void main(String[] args) {
-        // Ciphertext to be deciphered
-        String cipherText = "Tchiuy oFaiis chejaiin dxeedy qoPchegay keodaiiu otayaiin oar\n" +
-                "dar cheody okaiin odar okal okair oky daiir qotas okar olaiio\n" +
-                "todal eholky qokal shdy qoky otody qokyeea qokair pasy \n" +
-                "cnhor olkeedy olkchdy cheedy kaichdy chedaiib or choi karam \n" +
-                "daiib chekas olkaiib olkeody ykaiib otaib lar okeedy ykaso \n" +
-                "haiib chaky qoechdy otaly chedy dal dy chfhaiib chka qof\n" +
-                "pchedar osaiib eeeos kas or als acocthey olkaib aly\n" +
-                "yty qokaiub ykal chdy qoky osaib chy kaidy daiy\n" +
-                "otar chdy dytchdy";
+        String cipherText = "";
+
+        // Read ciphertext from a flat file
+        try {
+            File file = new File("vciphertext.txt");
+            Scanner fileScanner = new Scanner(file);
+            StringBuilder fileContent = new StringBuilder();
+
+            while (fileScanner.hasNextLine()) {
+                fileContent.append(fileScanner.nextLine()).append("\n");
+            }
+            fileScanner.close();
+            cipherText = fileContent.toString().toLowerCase().replaceAll("[^a-z]", "");
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: File 'ciphertext.txt' not found");
+            return; // Exit the program if the file is not found
+        }
 
         // "Clean" the ciphertext (remove spaces and make lowercase)
-        String ptext = cipherText.toLowerCase().replaceAll("[^a-z]", "");
-        System.out.println("\ni couldn't find a version of the manuscript that was translated using the little table on wikipedia you can see what" +
-                "im talking about on the image labeled 'what i used.jpeg' so i had to write out my whole page (page 94) by hand and compare each letter they " +
-                "used with the english alphebet. i used frequency analysis to get how frequent they use each letter then compared them to our most used." +
-                "when i did the analysis i got nothing back so i thought maybe it was shifted as well. so i did brute force with the ceasear cipher and looked at each one " +
-                "to see if any made sense. this still got me nothing back. i then thought maybe if i do it in italian since the origin is from italy maybe that would work" +
-                "but it wouldn't the letters i mapped to each letter from the manuscript was english so unless i wanted to completely do it over again using the italian alphebet" +
-                "then it wouldn't work, also i did think about redoing it in italian but i couldn't find anything like what i used like on the 'what i used.jpeg' capture" +
-                "i also put a bunch of examples of what it looked like when i was writing each letter from the manuscript into the english letter. ");
-                //^^ this is just me explaining my thought process i was gonna turn in a seperate document but didnt want you to have to open anything extra
+        String ptext = cipherText;
 
-        // Perform frequency analysis to map common letters
-        //https://github.com/joeoakes/javaBruteForceFreqAnalysis
+        // Perform frequency analysis and decryption for each language
+        processLanguage("English", ptext, new char[]{'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'});
+        processLanguage("Italian", ptext, new char[]{'e', 'a', 'i', 'o', 'n', 'r', 't', 'l', 's', 'c', 'd', 'u', 'm', 'p', 'v', 'g', 'f', 'b', 'q', 'z', 'h', 'x', 'j', 'k', 'y', 'w'});
+        processLanguage("Latin", ptext, new char[]{'u', 'e', 'i', 'a', 't', 'n', 'o', 'r', 's', 'c', 'l', 'm', 'p', 'd', 'g', 'b', 'v', 'h', 'q', 'f', 'x', 'k', 'z', 'y', 'j', 'w'});
+        processLanguage("Spanish", ptext, new char[]{'e', 'a', 'o', 's', 'n', 'r', 'i', 'l', 't', 'u', 'd', 'c', 'p', 'm', 'b', 'g', 'v', 'q', 'h', 'f', 'z', 'y', 'j', 'x', 'k', 'w'});
+    }
+
+    /**
+     * Processes the ciphertext for a given language.
+     * It performs frequency analysis and Caesar cipher brute force.
+     *
+     * @param language         The name of the language being processed.
+     * @param text             The cleaned ciphertext.
+     * @param letterFrequencies The letter frequency array for the language.
+     */
+    public static void processLanguage(String language, String text, char[] letterFrequencies) {
+        System.out.println("\n=== Processing for " + language + " ===");
+
         System.out.println("\nFrequency Analysis Results:");
-        String frequencyDecryptedText = performFrequencyAnalysis(ptext);
-//
-        // Perform Caesar cipher brute-force decryption on the frequency analysis result
-        //Security Hashing, HMAC, Brute Force, AES, TLS Presentation Joe Oaks  (BRUTE FORCE TO BE EXACT)
-        System.out.println("\nBrute Force Attempts on Frequency Analysis:");
+        String frequencyDecryptedText = performFrequencyAnalysis(text, letterFrequencies, language);
+
+        System.out.println("\nBrute Force Attempts:");
         for (int shift = 0; shift < 26; shift++) {
             String decrypted = caesarDecrypt(frequencyDecryptedText, shift);
             System.out.println("Shift " + shift + ": " + decrypted);
@@ -65,7 +67,6 @@ public class Voynich {
      * @param shift The shift amount for the cipher.
      * @return The decrypted text.
      */
-    //https://github.com/joeoakes/javaCaesarCipher \/
     public static String caesarDecrypt(String text, int shift) {
         StringBuilder result = new StringBuilder();
 
@@ -82,16 +83,14 @@ public class Voynich {
     }
 
     /**
-     * Analyzes letter frequencies in the text and tries to map them to common English letters.
+     * Analyzes letter frequencies in the text and tries to map them to common letters in a given language.
      *
-     * @param text The text to analyze.
+     * @param text               The text to analyze.
+     * @param languageFrequencies The letter frequencies for the language being analyzed.
+     * @param language           The name of the language being analyzed.
      * @return A decrypted version of the text based on frequency analysis.
      */
-    //    \/ https://github.com/joeoakes/javaBruteForceFreqAnalysis/blob/main/CaesarCipherFrequencyAnalysis.java
-    public static String performFrequencyAnalysis(String text) {
-        // Common English letter frequencies, most frequent first
-        char[] englishLetterFrequency = {'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'};
-
+    public static String performFrequencyAnalysis(String text, char[] languageFrequencies, String language) {
         // Count how often each letter appears
         Map<Character, Integer> frequencyMap = new HashMap<>();
         for (char c : text.toCharArray()) {
@@ -108,19 +107,17 @@ public class Voynich {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
 
-        // mapping the most common letters in the text to common English letters
-        System.out.println("\nVoynich to English Leters using Frequency Analysis:");
+        // Map the most common letters in the text to the language frequencies
+        System.out.println("\nVoynich to " + language + " Letters using Frequency Analysis:");
         Map<Character, Character> mapping = new HashMap<>();
         for (int i = 0; i < sortedFrequencies.size(); i++) {
-            if (i < englishLetterFrequency.length) {
-                mapping.put(sortedFrequencies.get(i).getKey(), englishLetterFrequency[i]);
-                System.out.println(sortedFrequencies.get(i).getKey() + " -> " + englishLetterFrequency[i]);
+            if (i < languageFrequencies.length) {
+                mapping.put(sortedFrequencies.get(i).getKey(), languageFrequencies[i]);
+                System.out.println(sortedFrequencies.get(i).getKey() + " -> " + languageFrequencies[i]);
             }
         }
 
         // Decrypt the text using the frequency map
-        //ngl chatgpt helped me on this part
-        System.out.println("\nDecrypted Text (Frequency Analysis):");
         StringBuilder decryptedText = new StringBuilder();
         for (char c : text.toCharArray()) {
             if (mapping.containsKey(c)) {
@@ -129,10 +126,11 @@ public class Voynich {
                 decryptedText.append(c); // Keep characters that don't match
             }
         }
+
         String result = decryptedText.toString();
+        System.out.println("\nDecrypted Text (Frequency Analysis):");
         System.out.println(result);
 
         return result;
-
     }
 }
